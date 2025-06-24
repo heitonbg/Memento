c8cace98aa9da7e8205ae6fea8b5099eaa45f51b
iwrawww@gmail.com <iwrawww@gmail.com>
2025-06-24 18:30:08 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Editor
+{
+    public string Content { get; set; }
+
+    public EditorMemento CreateMemento()
+    {
+        return new EditorMemento(Content);
+    }
+
+    public void RestoreMemento(EditorMemento memento)
+    {
+        Content = memento.SavedContent;
+    }
+
+    public void ShowContent()
+    {
+        Console.WriteLine($"Текущий текст: {Content}");
+    }
+}
+
+class EditorMemento
+{
+    public string SavedContent { get; }
+
+    public EditorMemento(string content)
+    {
+        SavedContent = content;
+    }
+}
+
+class History
+{
+    private Stack<EditorMemento> _mementos = new Stack<EditorMemento>();
+
+    public void SaveState(Editor editor)
+    {
+        _mementos.Push(editor.CreateMemento());
+    }
+
+    public void Undo(Editor editor)
+    {
+        if (_mementos.Count > 0)
+        {
+            editor.RestoreMemento(_mementos.Pop());
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var editor = new Editor();
+        var history = new History();
+
+        editor.Content = "Первая версия текста";
+        history.SaveState(editor);
+
+        editor.Content = "Измененный текст";
+        history.SaveState(editor);
+
+        editor.ShowContent(); 
+
+        history.Undo(editor);
+        editor.ShowContent(); 
+    }
+}
